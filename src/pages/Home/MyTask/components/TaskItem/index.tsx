7e32b83e442ef61d7a0cf9faf1d7ed8733ff7871
khiemld <khiemit240@gmail.com>
2024-01-10 22:03:07 +0700

    feat/K006: View current task

--- src/pages/Home/MyTask/components/TaskItem/index.tsx
+++ src/pages/Home/MyTask/components/TaskItem/index.tsx
@@ -1,28 +1,40 @@
-import { Dropdown, Tag } from 'antd'
+import { Button, Dropdown, Tag, Menu } from 'antd'
 import './style.scss'
 import { RiMore2Line } from 'react-icons/ri'
 import type { MenuProps } from 'antd'
 import { ITaskItemProps } from './ITaskItemProps'
 import dayjs from 'dayjs'
-
+import AddToCalendar from 'react-add-to-calendar'
 const TaskItem = (props: ITaskItemProps) => {
   const { task } = props
-  const handleClick = () => {
-    window.open(
-      `http://localhost:5173/u/boards/${task.board._id}/cards/${task._id}`,
-      '_blank'
-    )
-  }
-  const items: MenuProps['items'] = [
-    {
-      label: <div onClick={handleClick}>View Detail</div>,
-      key: '0'
-    },
-    {
-      label: <div>Add to Google Calendar</div>,
-      key: '1'
-    }
-  ]
+  // const handleClick = () => {
+  //   window.open(
+  //     `http://localhost:5173/u/boards/${task.board._id}/cards/${task._id}`,
+  //     '_blank'
+  //   )
+  // }
+
+  // const items: MenuProps['items'] = [
+  //   {
+  //     label: <div>View Detail</div>,
+  //     key: '0'
+  //   },
+  //   {
+  //     label: <div>Add to Calendar</div>,
+  //     key: '1'
+  //   }
+  // ]
+
+  // const event = {
+  //   description:
+  //     'Description of event. Going to have a lot of fun doing things that we scheduled ahead of time.',
+  //   duration: { hours: 1, minutes: 30 },
+  //   endDatetime: dayjs(task?.dueDate).format('YYYYMMDDTHHmmssZ'),
+  //   location: 'NYC',
+  //   startDatetime: dayjs(task?.startDate).format('YYYYMMDDTHHmmssZ'),
+  //   title: 'Super Fun Event'
+  // }
+
   return (
     <div className="upcomingTask-card">
       <div className="card-time">
@@ -51,20 +63,24 @@ const TaskItem = (props: ITaskItemProps) => {
             >
               {task?.priority}
             </Tag>
-            <Dropdown menu={{ items }} trigger={['click']}>
-              <RiMore2Line className="moreIcon"/>
-            </Dropdown>
           </div>
         </div>
-        <span className="card-content">
-          <b>Board:</b> {task?.board?.title}
-        </span>
-        <span className="card-content">
-          <b>Start Date:</b> {dayjs(task?.startDate).format('MMMM D, YYYY')}
-        </span>
-        <span className="card-content">
-          <b>Due Date:</b> {dayjs(task?.dueDate).format('MMMM D, YYYY')}
-        </span>
+        <div className="card-body">
+          <div className="card-content">
+            <div>
+              <b>Board:</b> {task?.board?.title}
+            </div>
+            <div>
+              <b>Start Date:</b> {dayjs(task?.startDate).format('MMMM D, YYYY')}
+            </div>
+            <div>
+              <b>Due Date:</b> {dayjs(task?.dueDate).format('MMMM D, YYYY')}
+            </div>
+          </div>
+          <div className="card-addCalendar">
+            
+          </div>
+        </div>
       </div>
     </div>
   )
